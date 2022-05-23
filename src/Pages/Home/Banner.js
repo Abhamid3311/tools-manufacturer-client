77ce463c845b9afebd8bfe66174e6c48e78d09a6
Hamid <www.abdulhamid3311@gmail.com>
2022-05-23 23:56:17 +0600

    solve all warning

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,28 +3,28 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <div class="carousel w-full">
-                <div id="slide1" class="carousel-item relative w-full">
-                    <img src='https://i.ibb.co/gvD4zNh/banner-1.jpg' class="w-full h-5/6" alt='' />
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src='https://i.ibb.co/gvD4zNh/banner-1.jpg' className="w-full h-5/6" alt='' />
 
                     /
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" class="btn btn-circle">❮</a>
-                        <a href="#slide2" class="btn btn-circle">❯</a>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide2" class="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/YTkXp0D/banner-3.jpg" class="w-full h-5/6" alt='' /> /
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" class="btn btn-circle">❮</a>
-                        <a href="#slide3" class="btn btn-circle">❯</a>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/YTkXp0D/banner-3.jpg" className="w-full h-5/6" alt='' /> /
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide3" class="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/d0pRBT9/banner-4-1.jpg" class="w-full h-5/6" alt='' /> /
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" class="btn btn-circle">❮</a>
-                        <a href="#slide4" class="btn btn-circle">❯</a>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/d0pRBT9/banner-4-1.jpg" className="w-full h-5/6" alt='' /> /
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
                     </div>
                 </div>
 
--- src/Pages/Home/Tools/Tool.js
+++ src/Pages/Home/Tools/Tool.js
@@ -4,16 +4,16 @@ const Tool = ({ tool, handleBuyNow }) => {
     const { id, name, model, made, price, quantity, mOrder, img } = tool;
     return (
         <div>
-            <div class="card w-90 bg-base-100 shadow-xl">
+            <div className="card w-90 bg-base-100 shadow-xl">
                 <figure><img src={img} alt="cars tool" className='h-52 w-full object-cover' /></figure>
-                <div class="card-body">
-                    <h2 class="card-title">{name}</h2>
-                    <h3 class="card-title">Model No. : {model}</h3>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <h3 className="card-title">Model No. : {model}</h3>
                     <p> <strong>Min-Order Quantity : {mOrder}</strong> </p>
                     <p>Available Quantity : {quantity}</p>
                     <h4 className='text-red-600'>Price: {price} BDT</h4>
-                    <div class="card-actions justify-end">
-                        <button onClick={() => handleBuyNow(id)} class="btn btn-accent text-white w-full">Buy Now</button>
+                    <div className="card-actions justify-end">
+                        <button onClick={() => handleBuyNow(id)} className="btn btn-accent text-white w-full">Buy Now</button>
                     </div>
                 </div>
             </div>
