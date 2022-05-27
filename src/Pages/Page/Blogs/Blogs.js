ed96eff7d19f50efd1aa6b3f170fe87c5f865c04
Hamid <www.abdulhamid3311@gmail.com>
2022-05-27 14:31:18 +0600

    added search field

--- src/Pages/Page/Blogs/Blogs.js
+++ src/Pages/Page/Blogs/Blogs.js
@@ -1,9 +1,67 @@
 import React from 'react';
 
 const Blogs = () => {
+    const products = [
+        {
+            name: "LG laptop",
+            price: 70000,
+            description: "This is a LED 15 inchis laptop",
+            made: "japan"
+        },
+        {
+            name: "LG mobile",
+            price: 25000,
+            description: "This is a 5 inchis mobile",
+            made: "japan"
+        },
+        {
+            name: "samsung laptop",
+            price: 90000,
+            description: "This is a LED 19 inchis laptop",
+            made: "korea"
+        },
+        {
+            name: "samsung galaxy mobile",
+            price: 25000,
+            description: "This is a super amoled display phone",
+            made: "korea"
+        },
+        {
+            name: "I-phone",
+            price: 160000,
+            description: "This is a patti wala phone",
+            made: "USA"
+        }
+    ];
+
+    const handleSearchDevice = e => {
+        const searchText = e.target.value;
+        console.log(searchText);
+        products.filter(product => product.includes('searchText'));
+
+    }
     return (
         <div className='m-10'>
             <h1 className='text-5xl text-center text-accent font-bold my-10'>Blog page</h1>
+
+
+            <div className='mb-10'>
+                <h2 className='text-3xl font-semibold mb-8'>You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</h2>
+
+                <div>
+                    <input
+                        onChange={handleSearchDevice}
+                        name='search'
+                        type="text"
+                        placeholder='search'
+                        class="input input-bordered input-primary w-full max-w-xs" />
+                    <div>
+                        {
+
+                        }
+                    </div>
+                </div>
+            </div>
 
             <div className='mb-10'>
                 <h2 className='text-3xl font-semibold'>How will you improve the performance of a React Application?</h2>
@@ -37,6 +95,8 @@ const Blogs = () => {
 
                 <p>Unit tests are typically automated tests written and run by software developers to ensure that a section of an application (known as the "unit") meets its design and behaves as intended.[2] In procedural programming, a unit could be an entire module, but it is more commonly an individual function or procedure. During development, a software developer may code criteria, or results that are known to be good, into the test to verify the unit's correctness. During test case execution, frameworks log tests that fail any criterion and report them in a summary. For this, the most commonly used approach is test - function - expected value.</p>
             </div>
+
+
 
         </div>
     );
