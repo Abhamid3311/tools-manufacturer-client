6ae41b93daf62da590c2cc99a5925893ca2caf34
Hamid <www.abdulhamid3311@gmail.com>
2022-06-16 10:41:48 +0600

    update banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -16,9 +16,9 @@ const Banner = () => {
         }} >
 
             <div class="hero-content text-center text-neutral-content">
-                <div class="max-w-lg mb-5">
-                    <h1 class="mb-5 text-4xl font-bold text-primary">WELCOME KHULNA TOOLS</h1>
-                    <p class="mb-5 text-black text-lg">If you work on your own vehicles, do yourself a favor and check out these cool tools. It's a collection that should be in every serious mechanic's tool chest.</p>
+                <div class=" mb-5">
+                    <h2 class="mb-5 text-4xl font-bold text-accent">WELCOME TO KHULNA TOOLS</h2>
+                    <p class="mb-5 text-black text-lg max-w-lg">If you work on your own vehicles, do yourself a favor and check out these cool tools. It's a collection that should be in every serious mechanic's tool chest.</p>
                     <button onClick={handleExplorer} class="btn btn-accent  text-lg">
                         <span className='mr-1'>Let's Explore</span> <AiOutlineArrowRight></AiOutlineArrowRight>
                     </button>
