058faf93bd8d870bd16b66303d2353ac685d4715
Hamid <www.abdulhamid3311@gmail.com>
2022-06-16 13:00:50 +0600

    added users page,

--- src/Pages/Page/Dashboard/MyProfile.js
+++ src/Pages/Page/Dashboard/MyProfile.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useForm } from "react-hook-form";
+import { useEffect } from 'react';
 
 const MyProfile = () => {
     const [user] = useAuthState(auth);
     const [users, setUsers] = useState({});
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
+    const { register, handleSubmit, reset } = useForm();
+    const [myProfile, setMyProfile] = useState({});
+    /* const [education, setEducation] = useState('');
+    const [phone, setPhone] = useState('');
+    const [city, setCity] = useState(''); */
 
+
+    //update User
+    const onSubmit = (data, e) => {
+        e.preventDefault();
         const userInfo = {
             userName: user?.displayName,
             userEmail: user?.email,
@@ -16,12 +24,10 @@ const MyProfile = () => {
             city: data.city,
             phone: data.phone
         };
-
-
         //send to server
-        const url = 'https://lit-retreat-00129.herokuapp.com/user';
+        const url = `http://localhost:5000/user/${user.email}`;
         fetch(url, {
-            method: 'POST',
+            method: 'PUT',
             headers: {
                 'content-type': 'application/json'
             },
@@ -33,39 +39,41 @@ const MyProfile = () => {
                 const newUser = [...users, result];
                 setUsers(newUser);
             })
+        reset();
     };
+
+    //Get User
+    useEffect(() => {
+        fetch(`http://localhost:5000/user/${user.email}`)
+            .then(res => res.json())
+            .then(data => setMyProfile(data))
+    }, [])
+
+
 
     return (
         <div className='w-1/2 mx-auto my-7 '>
-            <h2 className='text-4xl text-semibold text-accent mb-9'>My Profile</h2>
-            <div>
-                <h2 className='text-2xl'> Name: {user?.displayName}</h2>
-                <h3 className='text-2xl'>Email: {user?.email}</h3>
-                <h4 className='text-2xl'>Education: BBA In Dhaka University</h4>
-                <h4 className='text-2xl'>City: Dhaka </h4>
-                <h4 className='text-2xl'>Phone: 01730XXXXXXX</h4>
+            <div className='shadow-lg p-5 mb-14'>
+                <h2 className='text-4xl text-semibold text-accent mb-9'>My Profile</h2>
+                <div>
+                    <h2 className='text-2xl'> Name: {user?.displayName}</h2>
+                    <h3 className='text-2xl'>Email: {user?.email}</h3>
+                    <h4 className='text-2xl'>Education: {myProfile.education}</h4>
+                    <h4 className='text-2xl'>City: {myProfile.city} </h4>
+                    <h4 className='text-2xl'>Phone: {myProfile.phone}</h4>
+                </div>
             </div>
 
 
-            <h2 className='text-3xl text-semibold text-accent my-9'>Update Information</h2>
-            <form onSubmit={handleSubmit(onSubmit)} className="mt-10 mb-8">
-                <input {...register("education")} placeholder="education" class="input input-bordered input-primary w-full max-w-md mb-4" />
-                <input {...register("city")} placeholder="city" class="input input-bordered input-primary w-full max-w-md mb-4" />
-                <input {...register("phone")} placeholder="phone" class="input input-bordered input-primary w-full max-w-md mb-4" />
-                <input type="submit" value="Update" className='btn btn-accent' />
-            </form>
-
-
-
-
-
-
-
-
-
-
-
-
+            <div className='shadow-lg p-8'>
+                <h2 className='text-3xl text-semibold text-accent my-9'>Update Information</h2>
+                <form onSubmit={handleSubmit(onSubmit)} className="mt-10 mb-8">
+                    <input {...register("education")} placeholder="education" class="input input-bordered input-accent w-full max-w-md mb-4" />
+                    <input {...register("city")} placeholder="city" class="input input-bordered input-accent w-full max-w-md mb-4" />
+                    <input {...register("phone")} placeholder="phone" class="input input-bordered input-accent w-full max-w-md mb-4" />
+                    <input type="submit" value="Update" className='btn btn-accent' />
+                </form>
+            </div>
 
         </div>
     );
--- src/Pages/Page/Dashboard/Users.js
+++ src/Pages/Page/Dashboard/Users.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { AiFillDelete } from 'react-icons/ai';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user/')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
     return (
         <div>
-            <h2>Users</h2>
+            <h2 className='text-4xl text-semibold text-accent mb-9'>Users: {users.length}</h2>
+
+            <table class="table w-full">
+                <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+
+
+                {
+                    users.map(user => <tbody>
+                        <tr>
+                            <th>{user.userName}</th>
+                            <th>{user.userEmail}</th>
+                            <th>{user.phone}</th>
+
+                            <th><button className='btn btn-ghost text-2xl text-red-500'>
+                                <AiFillDelete></AiFillDelete>
+                            </button></th>
+                        </tr>
+                    </tbody>)
+
+
+                }
+            </table>
+            <div class="overflow-x-auto">
+
+            </div>
         </div>
     );
 };
